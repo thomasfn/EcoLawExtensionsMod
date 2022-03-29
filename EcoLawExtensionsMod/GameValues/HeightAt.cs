eed3cfb64ced4ae8d248c04a985f870d39d37118
Thomas Smith <thomasfn@fortfn.co.uk>
2022-03-29 14:59:12 +0100

    Added Height At and Layer Value At game values.

---
+++ EcoLawExtensionsMod/GameValues/HeightAt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Eco.Mods.LawExtensions
+{
+    using Core.Controller;
+    using Core.Utils;
+    using Core.Utils.PropertyScanning;
+
+    using Shared.Localization;
+    using Shared.Networking;
+    using Shared.Utils;
+    using Shared.Math;
+
+    using Gameplay.Civics.GameValues;
+
+    [Eco, LocCategory("World"), LocDescription("The Y coord of a location.")]
+    public class HeightAt : GameValue<float>
+    {
+        [Eco, Advanced, LocDescription("The position to read the Y coord of.")] public GameValue<Vector3i> Location { get; set; }
+
+        private Eval<float> FailNullSafeFloat<T>(Eval<T> eval, string paramName) =>
+            eval != null ? Eval.Make($"Invalid {Localizer.DoStr(paramName)} specified on {GetType().GetLocDisplayName()}: {eval.Message}", float.MinValue)
+                         : Eval.Make($"{Localizer.DoStr(paramName)} not set on {GetType().GetLocDisplayName()}.", float.MinValue);
+
+        public override Eval<float> Value(IContextObject action)
+        {
+            var location = this.Location?.Value(action); if (location?.Val == null) return this.FailNullSafeFloat(location, nameof(this.Location));
+            
+            var value = location.Val.Y;
+            return Eval.Make($"{Text.StyledNum(value)} (height above bedrock)", (float)value);
+        }
+        public override LocString Description() => Localizer.Do($"height above bedrock");
+    }
+}
---
+++ EcoLawExtensionsMod/GameValues/LayerValueAt.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace Eco.Mods.LawExtensions
+{
+    using Core.Controller;
+    using Core.Utils;
+    using Core.Utils.PropertyScanning;
+
+    using Shared.Localization;
+    using Shared.Networking;
+    using Shared.Utils;
+    using Shared.Math;
+
+    using Gameplay.Civics.GameValues;
+
+    using Simulation.WorldLayers;
+
+    [Eco, LocCategory("World"), LocDescription("The value of a world layer at a location.")]
+    public class LayerValueAt : GameValue<float>
+    {
+        [Eco, Advanced, LocDescription("The position to test.")] public GameValue<Vector3i> Location { get; set; }
+
+        [Eco, AllowNullInView, AllowEmpty, LocDescription("The world layer to read."), ] public string WorldLayer { get; set; }
+
+        private Eval<float> FailNullSafeFloat<T>(Eval<T> eval, string paramName) =>
+            eval != null ? Eval.Make($"Invalid {Localizer.DoStr(paramName)} specified on {GetType().GetLocDisplayName()}: {eval.Message}", float.MinValue)
+                         : Eval.Make($"{Localizer.DoStr(paramName)} not set on {GetType().GetLocDisplayName()}.", float.MinValue);
+
+        public override Eval<float> Value(IContextObject action)
+        {
+            var location = this.Location?.Value(action); if (location?.Val == null) return this.FailNullSafeFloat(location, nameof(this.Location));
+            if (string.IsNullOrEmpty(WorldLayer)) return Eval.Make($"Invalid WorldLayer specified on {GetType().GetLocDisplayName()}.", float.MinValue);
+
+            var layer = WorldLayerManager.Obj.GetLayer(WorldLayer);
+            if (layer == null) return Eval.Make($"Invalid WorldLayer specified on {GetType().GetLocDisplayName()}.", float.MinValue);
+
+            var value = layer[layer.WorldPosToLayerPos(new Vector2i(location.Val.X, location.Val.Z))];
+            return Eval.Make($"{Text.StyledNum(value)} (value of world layer {WorldLayer})", value);
+        }
+        public override LocString Description() => Localizer.Do($"value of world layer {WorldLayer}");
+    }
+}
